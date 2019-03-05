5119b934b78e66f54aba5cf45027f275baad436f
niki <clksyur@outlook.com>
2019-03-05 21:48:38 +0800

    1）将绝对路径修改为相对路径； 2）修改字典变量名

--- PYY2-1/PYY2-1.py
+++ PYY2-1/PYY2-1.py
@@ -1,7 +1,7 @@
 #【2-1】用python实现下面的功能: 允许用txt文件定义一个 词典，内容为一系列词的解释 如apple - food，每个词一行，你的python程序启动时加载一个词典，并开始接受命令行输入。当输入词典中的词时输出释义。如输入apple -输出food。词典中没有的词则输出unknown。
 
 #Python读取文本文档内容
-file_object = open(r"E:\Learning\Python-learning\PYY2-1\dict.txt",'r')
+file_object = open(r"dict.txt",'r')
 try:
      lines = file_object.readlines()
 finally:
@@ -14,29 +14,29 @@ print(lines)
 
 
 #将文本文档内容按要求存储在字典中
-d = {}
+dictionary = {}
 num = 0
 for word in lines:
     #if word != '\n':
      num+= 1
      if num % 2:    #偶数为key，奇数为value
-          d[lines[num-1]] = lines[num]
-print(d)
+          dictionary[lines[num-1]] = lines[num]
+print(dictionary)
 
 #dict中插入一个元素，试验一下
-d['respect'] = 'emotion'
-print(d['respect'])
+dictionary['respect'] = 'emotion'
+print(dictionary['respect'])
 #用法待学习
-for key in d.keys():
+for key in dictionary.keys():
     print('key = {}'.format(key))
 
-for value in d.values():
+for value in dictionary.values():
     print('value = {}'.format(value))
 
-for key,value in d.items():
+for key,value in dictionary.items():
     print('{key} : {value}'.format(key = key, value = value))
 
 
 #接受命令行输入，当输入词典中的词时输出释义。词典中没有的词则输出unknown。
 word = input('Please input the word：')
-print(d.get(word,'unknown'))
+print(dictionary.get(word,'unknown'))
